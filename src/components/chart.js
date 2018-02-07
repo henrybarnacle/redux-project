68577b197d215ab6a206f32625edd48694678ce3
Henry Barnacle <henrybarnacle@yahoo.com>
2018-02-07 12:32:14 -0500

    adding google maps

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,11 +1,18 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+	return _.round(_.sum(data)/data.length);
+}
 
 export default (props) =>  {
 return (
 	<div>
-		<Sparklines height={120} width={180} data={props.data}>
+		<Sparklines height={140} width={180} data={props.data}>
 			<SparklinesLine color={props.color} />
+			<SparklinesReferenceLine type="avg"> </SparklinesReferenceLine>
+			<div> Average: {average(props.data)} {props.units}</div>
 		</Sparklines>
 	</div>
 	);
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,34 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
 	renderWeather(cityData) {
 		const name = cityData.city.name;
-		const temps = cityData.list.map(weather => weather.main.temp);
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp -273);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const {lon, lat } = cityData.city.coord;
 
-		console.log(temps);
+
 		return (
 				<tr key={name}>
-					<td>{name}</td>
-					<td>
-						<Chart data={temps} color="orange" />
-					</td>
+					<td><GoogleMap lon={lon} lat={lat} /></td>
 
+					<td><Chart data={temps} color="orange" units="c" /></td>
+					<td><Chart data={pressures} color="green" units="hPa"/></td>
+					<td><Chart data={humidities} color="black" units="%"/></td>
+					
 				</tr>
 			);
 	}
 
 	render() {
 		return (
-			<table className="table table-hover">
+			<table className="table">
 			<thead>
 			<tr>
 				<th>City</th>
-				<th>Temperature</th>
-				<th>Pressure</th>
-				<th>Humidity</th>
+				<th>Temperature (c)</th>
+				<th>Pressure (hPa)</th>
+				<th>Humidity (%)</th>
+				
 			</tr>
 			</thead>
 			<tbody> 
